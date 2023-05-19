b3d2fa2b1a9d04e81ad844a61028428eab88a0c7
Shohidul Arifin <shohidularifin@gmail.com>
2023-05-19 06:25:53 -0700

    AuthProvider added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,7 +3,43 @@
 const Login = () => {
     return (
         <div>
-            <h2>This is login page</h2>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col ">
+                    <div className="text-center ">
+                        <h1 className="text-5xl font-bold">Please Login!</h1>
+
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <form onSubmit={handleLogin} className="card-body">
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="email" name='email' placeholder="email" className="input input-bordered" required />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Login</button>
+                            </div>
+                        </form>
+                        <Link to='/register'>
+                            <button className="btn btn-link">New to car planet? Please Register</button>
+                        </Link>
+                        <button onClick={handleGoogleSignIn} className='bg-red-300 p-2 text-white rounded'>Google</button>
+                       
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,58 @@
+import {  createContext, useEffect, useState } from "react";
+import { GoogleAuthProvider, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import app from "../firebase/firebase.config";
+
+const auth = getAuth(app);
+const googleAuthProvider = new GoogleAuthProvider();
+export const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+    const[user, setUser]= useState(null);
+    const[loading,setLoading]= useState(true);
+    const createUser =(email,password)=>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+
+    const signIn= (email,password)=>{
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+
+    const signInWithGoogle=()=>{
+        setLoading(true);
+        return signInWithPopup(auth,googleAuthProvider )
+    }
+
+    const logOut =()=>{
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser=>{
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return()=>{
+            unsubscribe();
+        }
+    },[])
+
+        
+    const authInfo = {
+        user,
+        createUser,
+        signIn,
+        signInWithGoogle,
+        logOut,
+        loading
+    }
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
